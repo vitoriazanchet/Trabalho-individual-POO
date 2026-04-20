45f6cfc22615a796f9382be066d6cc4693582fff
vitoriazanchet <vitoria.zanchet@hotmail.com>
2026-04-20 20:01:02 -0300

    Exercício 1

---
+++ src/Exercicio1/CaixaEletronico.java
@@ -0,0 +1,54 @@
+package Exercicio1;
+
+import java.util.Scanner;
+
+public class CaixaEletronico{
+    public static void main(String[] args){
+        Scanner teclado = new
+                Scanner(System.in);
+        double saldo = 100.00;
+        int contadorSaques = 0;
+        int opcao;
+        do {
+            System.out.println("\n***CAIXA ELETRÔNICO***\n" +
+                    "\nEscolha a operação que deseja realizar:\n" +
+                    "1 - Ver Saldo\n2 - Depositar\n3 - Sacar\n0 - Sair\n" +
+                    "\nDigite o número da opção correspondente: ");
+                    opcao = teclado.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Saldo atual: R$" + saldo + "\n");
+                    break;
+                case 2:
+                    System.out.println("Digite o valor que deseja depositar: ");
+                    double deposito = teclado.nextDouble();
+                    saldo += deposito;
+                    System.out.println("Depósito realizado!\n");
+                    break;
+                case 3:
+                    if (contadorSaques >= 3) {
+                        System.err.println("Limite de saques diários atingido!\n");
+                    } else {
+                        System.out.println("Digite o valor que deseja sacar: ");
+                        double saque = teclado.nextDouble();
+                        if (saque > 1000) {
+                            System.err.println("Valor máximo por saque é R$ 1.000,00.\n");
+                        } else if (saque > saldo) {
+                            System.err.println("Saldo insuficiente!\n");
+                        } else {
+                            saldo -= saque;
+                            contadorSaques++;
+                            System.out.println("Saque realizado com sucesso!\n");
+                        }
+                    }
+                    break;
+                case 0:
+                    System.out.println("Sistema encerrado.");
+                    break;
+                default:
+                    System.err.println("Opção inválida!\n");
+            }
+        } while (opcao != 0);
+        teclado.close();
+    }
+}
